5cf71675dea0cdcb2bc9a27a06482fc1f0f2a1fc
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-07-13 14:44:44 +0200

    Add generateEmbeddings utility

--- lib/ai/embeddings.ts
+++ lib/ai/embeddings.ts
@@ -1,6 +1,26 @@
+import { openai } from "@ai-sdk/openai";
+import { embedMany } from "ai";
+
+const embeddingModel = openai.embedding("text-embedding-ada-002");
+
 const generateChunks = (text: string): Array<string> => {
     return text
         .trim()
         .split(".")
         .filter(chunk => chunk !== "");
+}
+
+export const generateEmbeddings = async (
+    text: string
+): Promise<Array<{ embedding: Array<number>, content: string}>> => {
+    const chunks = generateChunks(text);
+    const { embeddings } = await embedMany({
+        model: embeddingModel,
+        values: chunks
+    });
+
+    return embeddings.map((e, i) => ({
+        embedding: e,
+        content: chunks[i],
+    }));
 }
