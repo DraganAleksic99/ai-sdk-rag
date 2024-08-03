269e3884b635717f9b61dd3418dc8c9b43341b11
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-08-03 14:42:33 +0200

    Update: add findRelevantContent function

--- lib/ai/embeddings.ts
+++ lib/ai/embeddings.ts
@@ -1,5 +1,8 @@
 import { openai } from "@ai-sdk/openai";
 import { embed, embedMany } from "ai";
+import { cosineDistance, desc, gt, sql } from "drizzle-orm";
+import { db } from "../db";
+import { embeddings } from "@/lib/db/schema/embeddings";
 
 const embeddingModel = openai.embedding("text-embedding-ada-002");
 
@@ -35,3 +38,20 @@ const generateEmbedding = async (value: string): Promise<number[]> => {
 
     return embedding;
 }
+
+export const findRelevantContent = async (userQuery: string) => {
+    const userQueryEmbedded = await generateEmbedding(userQuery);
+    const similarity = sql<number>`1 - (${cosineDistance(
+      embeddings.embedding,
+      userQueryEmbedded,
+    )})`;
+
+    const similarGuides = await db
+      .select({ name: embeddings.content, similarity })
+      .from(embeddings)
+      .where(gt(similarity, 0.5))
+      .orderBy(t => desc(t.similarity))
+      .limit(4);
+      
+    return similarGuides;
+  };
